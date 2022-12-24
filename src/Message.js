a2636095c577c738edc58b68affa15eb4fe01ffd
bingual <bingual76@gmail.com>
2022-12-24 10:54:41 +0900

    CSS를 적용하는 다양한 방법 (sass 활용)

---
+++ src/Message.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Message.module.sass';
+
+const Message = () => <div className={styles.wrapper}>Message Component</div>;
+
+export default Message;
---
+++ src/Profile.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Profile.module.sass';
+
+const Profile = () => <div className={styles.wrapper}>Profile Component</div>;
+
+export default Profile;
